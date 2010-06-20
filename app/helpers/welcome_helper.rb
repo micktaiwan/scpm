d18a40082a158441ad3f286b937179593b96e09a
Mickael Faivre-Maçon <faivrem@gmail.com>
2010-06-20 20:53:42 +0000

    better reports

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,14 @@
 module WelcomeHelper
+
+
+  def report_by(type, rs)
+    rv  = "<a href='#' onclick=\"$('assigned_to_#{type}').toggle();return false;\">"
+    rv += (type + ": " + rs.size.to_s + "</a><br/>")
+    rv += "<ul id='assigned_to_"+type+"' style='display:none'>"
+    rs.each { |r|
+      rv += ("<li>" + r.project + ": " + r.summary_project_name + " " + r.work_package  + " " +  r.status + "</li>")
+      }
+    rv += "</ul>"
+  end      
+
 end
