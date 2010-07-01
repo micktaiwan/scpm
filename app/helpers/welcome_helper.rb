c78e8508abde76eb8bcd02da89e87c56a460d272
Mickael Faivre-Maçon <faivrem@gmail.com>
2010-07-01 16:32:19 +0000

    more work

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -21,7 +21,7 @@ module WelcomeHelper
     rv += "<ul id='"+id+"_"+title+"' style='display:none'>"
     rv += "<table><tr class='theader'><td>#</td><td>WS</td><td>Project</td><td>PM</td><td>Type</td><td>Miles.</td><td>Status</td><td>Resp</td><td>Load</td><td>Start date</td><td>Progress</td><td>SDP</td></tr>"
     rs.each { |r|
-      rv += ("<tr class='#{sanitize(r.status)}'><td>#{r.request_id.to_i}</td><td>#{r.workstream}</td><td><a href='http://toulouse.sqli.com/EMN/view.php?id=#{r.request_id.to_i}'>" + r.summary + "</a></td><td>#{r.pm}</td><td>#{r.work_package}</td><td>#{r.milestone}</td><td><b>" +  r.status + "</b></td><td>#{r.assigned_to}</td><td>#{r.workload}</td><td>#{r.start_date.to_s}</td><td>#{r.resolution}<td>")
+      rv += ("<tr class='#{sanitize(r.status)}'><td>#{r.request_id.to_i}</td><td>#{r.workstream}</td><td><a href='http://toulouse.sqli.com/EMN/view.php?id=#{r.request_id.to_i}'>" + r.summary + "</a></td><td>#{r.pm}</td><td>#{r.work_package}</td><td>#{r.milestone}</td><td><b>#{r.status}</b></td><td>#{r.assigned_to}</td><td>#{r.workload}</td><td>#{r.start_date.to_s}</td><td class='status_#{sanitize(r.resolution)}'>#{r.resolution}<td>")
       # TODO: write an helper in the request model
       rv += "<b>" if r.sdp == "No"
       rv += " SDP:" + r.sdp if r.sdp
