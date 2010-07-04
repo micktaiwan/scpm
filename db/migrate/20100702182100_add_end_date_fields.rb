506204fe55e1daeae5de0d2445e8183005fbc976
Mickael Faivre-Maçon <faivrem@gmail.com>
2010-07-04 11:23:24 +0000

    minor

--- db/migrate/20100702182100_add_end_date_fields.rb
+++ db/migrate/20100702182100_add_end_date_fields.rb
@@ -1,9 +1,11 @@
 class AddEndDateFields < ActiveRecord::Migration
   def self.up
     add_column :requests, :actual_m_date, :string
+    add_column :requests, :end_date, :string
   end
 
   def self.down
     remove_column :requests, :actual_m_date
+    remove_column :requests, :end_date
   end
 end
