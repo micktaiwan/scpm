da91a53520972c4565f1a52a1d1330c35bcb6d00
Mickael Faivre-Maçon <faivrem@gmail.com>
2010-09-14 19:45:21 +0000

    fix

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,7 +1,7 @@
 module ProjectsHelper
 
   def are_filtered
-    session[:project_filter_workstream] or session[:project_filter_status] or session[:project_filter_supervisor]
+    session[:project_filter_workstream] or session[:project_filter_status] or session[:project_filter_supervisor] or session[:project_filter_qr]
   end
 
 end
