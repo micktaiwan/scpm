bb8273cebdaebd5c3d8d5fa0f6ae96fe35a98cfe
Mickael Faivre-Maçon <faivrem@gmail.com>
2010-10-15 16:24:18 +0000

    more excel report

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -38,7 +38,33 @@ module ProjectsHelper
       when 3; "<span class='status red'>red</span>"
     end
   end
+  
+  def text_status(s)
+    case s
+      when 0; "Unknown"
+      when 1; "GREEN"
+      when 2; "AMBER"
+      when 3; "RED"
+    end
+  end
 
+  def status_excel_style1(s)
+    case s
+      when 0; 's77'
+      when 1; 's78'
+      when 2; 's79'
+      when 3; 's80'
+    end
+  end
+
+  def status_excel_style2(s)
+    case s
+      when 0; 's81'
+      when 1; 's82'
+      when 2; 's83'
+      when 3; 's84'
+    end
+  end
 
 end
 
