4600b976c3a3973805520d547d83b3fcc7522266
Mickael Faivre-Maçon <faivrem@gmail.com>
2010-11-21 12:40:45 +0000

    better styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,5 +6,13 @@ module ApplicationHelper
   def my_simple_format(text)
     text.split("\n").join("<br/>") + "<br/>"
   end
-  
+
+  def menu_style(c,a)
+    if c == controller_name and a == action_name
+      'menu active'
+    else
+      'menu'
+    end
+  end
+
 end
