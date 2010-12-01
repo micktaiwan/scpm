925dd89528d626d268da1dda36d7ac58d571d456
Mickael Faivre-Maçon <faivrem@gmail.com>
2010-12-01 21:52:35 +0000

    better actions display (closed actions are first hidden)

--- app/controllers/actions_controller.rb
+++ app/controllers/actions_controller.rb
@@ -3,8 +3,10 @@ class ActionsController < ApplicationController
   before_filter :require_login
 
   def index
-    @actions = Action.find(:all, :conditions=>["person_id=?", current_user.id], :order=>"progress, project_id, creation_date")
-    @other_actions = Action.find(:all, :conditions=>["person_id!=?",current_user.id], :order=>"progress, project_id, creation_date")
+    @actions              = Action.find(:all, :conditions=>["person_id=? and progress in('open','in_progress')", current_user.id], :order=>"progress, creation_date")
+    @actions_closed       = Action.find(:all, :conditions=>["person_id=? and progress in('closed','abandonned')", current_user.id], :order=>"progress, creation_date")
+    @other_actions        = Action.find(:all, :conditions=>["person_id!=? and progress in('open','in_progress')",current_user.id], :order=>"progress, creation_date")
+    @other_actions_closed = Action.find(:all, :conditions=>["person_id!=? and progress in('closed','abandonned')",current_user.id], :order=>"progress, creation_date")
   end
 
   def new
