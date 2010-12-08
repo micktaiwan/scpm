40970ec47d552dc5d0db5779904519e08447558d
Mickael Faivre-Maçon <faivrem@gmail.com>
2010-12-08 18:57:44 +0000

    adding nb of projects stats by workstream

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -11,6 +11,8 @@ class ToolsController < ApplicationController
     year      = 2010
     @xml = Builder::XmlMarkup.new(:indent => 1)
     @stats = []
+
+    # global stats
     begin
       d =  Date.new(year,month,1) - 1.day
       requests = Request.find(:all, :conditions=>"date_submitted < '#{d.to_s}' and status!='to be validated' and status!='cancelled'")
@@ -29,6 +31,31 @@ class ToolsController < ApplicationController
       return
     end while year < today.cwyear or (year == today.cwyear and month <= today.month)
 
+    # by centres
+    @centres = []
+    for centre in ["EA", "EDE", "EV", "EDC", "EDG", "EDS", "EI", "EDY", "EM", "EMNB", "EMNC"] do
+      month     = 6
+      year      = 2010
+      stats = Array.new
+      @centres << {:centre=>centre, :stats=>stats}
+      begin
+        d =  Date.new(year,month,1) - 1.day
+        requests = Request.find(:all, :conditions=>"workstream='#{centre}' and date_submitted < '#{d.to_s}' and status!='to be validated' and status!='cancelled'")
+        a = requests.size
+        b = requests.map{|r| r.project_name}.uniq.size # work also with a simple group by clause
+        c = requests.map{|r| get_workpackage_name_from_summary(r.summary, '')}.uniq.size
+        stats << [d, a,b,c]
+
+        month += 1
+        if month > 12
+          month = 1
+          year += 1
+        end
+      rescue Exception => e
+        render(:text=>"<b>#{e}</b><br>#{e.backtrace.join("<br>")}")
+        return
+      end while year < today.cwyear or (year == today.cwyear and month <= today.month)
+    end
     headers['Content-Type'] = "application/vnd.ms-excel"
     headers['Content-Disposition'] = 'attachment; filename="Stats.xls"'
     headers['Cache-Control'] = ''
