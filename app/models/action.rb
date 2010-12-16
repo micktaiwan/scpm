a41feaf967330afc00caa49172afd1d2f29530c3
Mickael Faivre-Maçon <faivrem@gmail.com>
2010-12-16 07:32:25 +0000

    fix

--- app/models/action.rb
+++ app/models/action.rb
@@ -20,9 +20,9 @@ private
   def track_changes
     was_p = Project.find(self.project_id_was)
     if was_p
-      self.result = (self.result? self.result : "") + " changed from project #{self.project_id_was}, #{was_p.name}" if self.project_id_changed? and self.project_id_was
+      self.result = (self.result ? self.result : "") + " changed from project #{self.project_id_was}, #{was_p.name}" if self.project_id_changed? and self.project_id_was
     else
-      self.result = (self.result? self.result : "") + " changed from project #{self.project_id_was}, deleted" if self.project_id_changed? and self.project_id_was
+      self.result = (self.result ? self.result : "") + " changed from project #{self.project_id_was}, deleted" if self.project_id_changed? and self.project_id_was
     end    
   end
   
