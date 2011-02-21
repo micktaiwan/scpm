f7ec85886f7b6418461f4d64ff3cd6f4a871b0f3
Mickael Faivre-Maçon <faivrem@gmail.com>
2011-02-21 08:54:31 +0000

    fix last_change_excel

--- app/models/status.rb
+++ app/models/status.rb
@@ -5,6 +5,10 @@ class Status < ActiveRecord::Base
   def is_current?
     self.updated_at.to_date.cweek == Date.today.cweek
   end
+  
+  def get_last_change_excel
+    return self.last_change_excel if self.last_change_excel
+    self.last_change
+  end
 
 end
-
