e4f1bf890ab68390efe5fc3b4c6681babbe5d2f8
Mickael Faivre-Maçon <faivrem@gmail.com>
2011-06-01 09:53:06 +0000

    LDAP is back again. Some status text updates.

--- app/models/person.rb
+++ app/models/person.rb
@@ -67,7 +67,7 @@ class Person < ActiveRecord::Base
   def self.authenticate(login, password)
     return self.find_by_login(login[4..-1]) if login[0..3] == "test" and password=="sqlitlse"
     return nil if login.empty? or password.empty?
-    return self.find_by_login(login) # temporaire.....
+    #return self.find_by_login(login) # temporaire.....
     
     if login_by_ldap(login,password)
       self.find_by_login(login)
