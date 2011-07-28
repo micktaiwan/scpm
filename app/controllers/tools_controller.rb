ec4ade4bab2a8278f7bb4952ec55b00cc91144e5
Mickael Faivre-Maçon <faivrem@gmail.com>
2011-07-28 15:44:20 +0000

    update sdp index

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -130,7 +130,7 @@ class ToolsController < ApplicationController
       @management_minus_risk        = @remaining_management - (@provisions_remaining + @risks_remaining)
       @real_balance_and_provisions  = @provisions_diff+@balancei-diff_theorical_actual
       @real_balance                 = @real_balance_and_provisions - @provisions_remaining_should_be
-      @overall_remaining            = diff_theorical_actual + @ci_remaining + @qa_remaining #+ (montee+souscharges+init)
+      @remaining_DP                 = diff_theorical_actual + @ci_remaining + @qa_remaining #+ (montee+souscharges+init)
     rescue Exception => e
       render(:text=>"<b>Error:</b> <i>#{e.message}</i>")
     end
