f393fd91939488f2caa37f6946244cfecc3f0e51
Mickael Faivre-Maçon <faivrem@gmail.com>
2011-09-06 17:06:49 +0000

    starting unit tests for checklist templates

--- test/unit/checklist_item_test.rb
+++ test/unit/checklist_item_test.rb
@@ -1,8 +1,16 @@
 require 'test_helper'
 
 class ChecklistItemTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  test "milestone request" do
+    checklist_item_templates(:dn).workpackages.include?(workpackages(:assurance))
+    checklist_item_templates(:dn).milestone_names.include?(milestone_names(:m5))
+    assert checklist_item_templates(:dn).requests.include?(requests(:request))
+    #assert checklist_item_templates(:pp).requests.include?(requests(:request))
+  end
+
+  test "deployment test" do
+    checklist_item_templates(:dn).deploy
+    assert milestones(:milestone).checklist_items.size > 0
   end
 end
