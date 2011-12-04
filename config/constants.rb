994ddda44afc1e3c91372fdef1174ddf6ae40a84
Mickael Faivre-Maçon <faivrem@gmail.com>
2011-12-04 15:53:28 +0000

    fix

---
+++ config/constants.rb
@@ -0,0 +1,15 @@
+# operational team weekly meetings
+# see /tools/import_monthly_tasks_form
+SDP_MONTHLY_IMPORT_Phase_ID       = 29255 # Bundle_Management
+SDP_MONTHLY_IMPORT_Activity_ID    = 127647 # Bundle_Management_during_the_project
+SDP_MONTHLY_IMPORT_Ref_Task_ID    = 110 # Encadrement
+SDP_MONTHLY_IMPORT_Macro_Task_ID  = -1
+SDP_MONTHLY_IMPORT_Workpackage_ID = -1
+SDP_MONTHLY_IMPORT_Iteration_ID   = 3056 # HO
+SDP_MONTHLY_IMPORT_Entity_ID      = -1
+SDP_MONTHLY_IMPORT_Team_ID        = -1
+SDP_MONTHLY_IMPORT_Domain_ID      = -1
+SDP_MONTHLY_IMPORT_Agency_ID      = 13 # Toulouse
+SDP_MONTHLY_IMPORT_Proposal_Phase_ID = 28067 # Bundle Management
+SDP_MONTHLY_IMPORT_Comments       = "Internal SQLI Meetings (imported from BAM)"
+
