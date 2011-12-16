ee5d7f0cf78e7a1a7e03a3cb922aebddac0e6288
Mickael Faivre-Maçon <faivrem@gmail.com>
2011-12-16 07:13:41 +0000

    fix

--- app/models/project.rb
+++ app/models/project.rb
@@ -25,7 +25,7 @@ class Project < ActiveRecord::Base
   has_many    :project_people
   has_many    :responsibles, :through=>:project_people
   has_many    :risks,       :order=>'id', :dependent=>:destroy
-  has_many    :qauality_risks,  :class_name=>"Risk", :foreign_key=>"project_id", :order=>'id', :dependent=>:destroy, :conditions=>"is_quality=1"
+  has_many    :quality_risks,  :class_name=>"Risk", :foreign_key=>"project_id", :order=>'id', :dependent=>:destroy, :conditions=>"is_quality=1"
   has_many    :checklist_items, :through=>:milestones
   has_many    :project_check_items, :class_name=>"ChecklistItem"
   has_many    :project_check_root_items, :conditions=>"parent_id=0", :class_name=>"ChecklistItem"
