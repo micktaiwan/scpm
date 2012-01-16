188a86ef467fd7d6cbf1989fb7a1138774b17040
Mickael Faivre-Maçon <faivrem@gmail.com>
2012-01-16 14:10:00 +0000

    adding open checklists to daily reminder

--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -42,7 +42,7 @@ class Mailer < ActionMailer::Base
     render(:nothing=>true) and return if !person or person.email.empty?
     @from        = "mfaivremacon@sqli.com"
     @recipients  = "#{person.email}, mfaivremacon@sqli.com"
-    @subject     = "[BAM] Daily reminders (#{person.name})"
+    @subject     = "[BAM] Reminders for #{person.name}"
     @person, @new_notes, @requests_to_close, @amendments, @actions, @milestones_with_open_checklists = person, n, r, am, ac, om
     content_type "text/html; charset=utf-8"
   end
