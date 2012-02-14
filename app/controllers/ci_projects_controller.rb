440ba00521bf17de91277177aa8de09556a46ee1
Mickael Faivre-Maçon <faivrem@gmail.com>
2012-02-14 10:31:22 +0000

    more work on CI management

--- app/controllers/ci_projects_controller.rb
+++ app/controllers/ci_projects_controller.rb
@@ -1,8 +1,10 @@
+require 'lib/csv_ci'
 class CiProjectsController < ApplicationController
 
 	layout 'ci'
 
 	def index
+			@projects = CiProject.find(:all,:order=>"status, external_id")
 	end
 
   def do_upload
@@ -12,14 +14,14 @@ class CiProjectsController < ApplicationController
     directory = "public/data"
     path = File.join(directory, name)
     File.open(path, "wb") { |f| f.write(post['datafile'].read) }
-    report = CvsCiReport.new(path)
+    report = CsvCiReport.new(path)
     report.parse
     # transform the Report into a CiProject
-    report.requests.each { |p|
+    report.projects.each { |p|
       # get the id if it exist, else create it
       ci = CiProject.find_by_external_id(p.external_id)
       ci = CiProject.create(:external_id=>p.exterbal_id) if not ci
-      ci.update_attributes(ci.to_hash) # and it updates only the attributes that have changed !
+      ci.update_attributes(p.to_hash) # and it updates only the attributes that have changed !
       ci.save
       }
     redirect_to '/ci_projects/index'
---
+++ lib/csv_ci.rb
@@ -0,0 +1,114 @@
+require 'csv'
+
+class CsvCi
+
+  attr_accessor  :internal_id,
+    :external_id,
+    :stage,
+    :category,
+    :severity,
+    :summary,
+    :description,
+    :status,
+    :submission_date,
+    :reporter,
+    :last_update,
+    :last_update_person,
+    :assigned_to,
+    :priority,
+    :visibility,
+    :resolution_charge,
+    :additional_information,
+    :taking_into_account_date,
+    :realisaton_date,
+    :realisation_author,
+    :delivery_date,
+    :origin,
+    :improvement_target_objective,
+    :scope_l2,
+    :deliverable_list,
+    :accountable,
+    :deployment,
+    :launching_date,
+    :validation_date_objective,
+    :airbus_validation_date_objective,
+    :deployment_date_objective,
+    :sali_validation_date,
+    :airbus_validation_date,
+    :deployment_date,
+    :deployment_date_review
+
+  def initialize
+  end
+
+  def method_missing(m, *args, &block)
+    #raise "CsvCi does not have a '#{m}' attribute/method"
+  end
+
+  def to_hash
+    h = Hash.new
+    self.instance_variables.each { |var|
+      h[var[1..-1].to_sym] = self.instance_variable_get(var)
+      }
+    h
+  end
+end
+
+class CsvCiReport
+
+  attr_reader :projects
+
+  def initialize(path)
+    @path = path
+    @projects = []
+    @columns = Hash.new
+  end
+
+  def parse
+    reader = CSV.open(@path, 'r')
+    get_columns(reader.shift)
+    while not (row = reader.shift).empty?
+      parse_row(row)
+    end
+  end
+
+private
+
+  def get_columns(row)
+    row.each_with_index { |r,i|
+      @columns[sanitize_attr(r)] = i
+      #puts sanitize_attr(r)
+      }
+  end
+
+  def parse_row(row)
+    r = CsvCi.new
+    @columns.each { |attr_name, index|
+      #puts "#{attr_name} = '#{row[index]}'"
+      eval("r.#{attr_name} = \"#{sanitize_value(row[index])}\"")  # r.id = row[1]
+      }
+    @projects << r
+  end
+
+  def sanitize_value(value)
+    return nil if !value
+    value.gsub!("\"","'")
+    value
+  end
+
+  def sanitize_attr(name)
+    name = name.downcase
+    name.gsub!(" #","")
+    name.gsub!("/","")
+    name.gsub!("  ","_")
+    name.gsub!(" ","_")
+    name.gsub!("-","_")
+    name.gsub!(".","")
+    name.gsub!(/\d\d\_/,"")
+    name.gsub!(/\_\d\d/ ,"")
+    name = "internal_id" if name == "internal"
+    name = "external_id" if name == "id"
+    name
+  end
+
+end
