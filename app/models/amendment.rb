7979e23fcc373f37d80b98b7b8b3615464f74007
cdebortoli <cdebortoli@sqli.com>
2012-09-06 18:21:11 +0200

    Fix amendments creation

--- app/models/amendment.rb
+++ app/models/amendment.rb
@@ -4,9 +4,16 @@ class Amendment < ActiveRecord::Base
   before_save :check_done_status
   
   def check_done_status
-    previous_amendment = Amendment.find(self.id)
-    if previous_amendment.done == 0 and self.done == 1
+    
+    if self.id
+      previous_amendment = Amendment.find(self.id)
+      if previous_amendment.done == 0 and self.done == 1
+        self.done_date = Date.today
+      end
+    elsif self.done == 1
       self.done_date = Date.today
     end
+    
   end
+  
 end
