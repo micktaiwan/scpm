8f8017ebf2a3ca4f8d44d71db334133e0ae852ca
cdebortoli <cdebortoli@sqli.com>
2012-11-19 15:24:34 +0100

    Question references by axes.

--- app/controllers/question_references_controller.rb
+++ app/controllers/question_references_controller.rb
@@ -1,15 +1,23 @@
 class QuestionReferencesController < ApplicationController
   layout "tools"
   def index
-    
-    @questionReferencesList = QuestionReference.find(:all,
-    :joins => ["JOIN lifecycle_questions ON lifecycle_questions.id = question_references.question_id",
-               "JOIN pm_type_axes ON lifecycle_questions.pm_type_axe_id = pm_type_axes.id",
-               "JOIN pm_types ON pm_type_axes.pm_type_id = pm_types.id",
-               "JOIN lifecycles ON lifecycle_questions.lifecycle_id = lifecycles.id",
-               "JOIN milestone_names ON question_references.milestone_id = milestone_names.id"])
+    @axesList = PmTypeAxe.find(:all).map{|u| [u.title,u.id]}
+    @firstAxeId = @axesList.first[1]
   end
 
+  def generate_question_references_table
+    axe_id = params[:axe_id]
+    @questionReferencesList = QuestionReference.find(:all,
+      :joins => ["JOIN lifecycle_questions ON lifecycle_questions.id = question_references.question_id",
+                 "JOIN pm_type_axes ON lifecycle_questions.pm_type_axe_id = pm_type_axes.id",
+                 "JOIN pm_types ON pm_type_axes.pm_type_id = pm_types.id",
+                 "JOIN lifecycles ON lifecycle_questions.lifecycle_id = lifecycles.id",
+                 "JOIN milestone_names ON question_references.milestone_id = milestone_names.id"],
+      :conditions => ["pm_type_axes.id = ?",axe_id.to_s])
+      render(:layout=>false)
+  end
+  
+  
   def save_question_reference
     question_reference_id = params[:question_reference_id]
     question_reference_note = params[:question_reference_note]
