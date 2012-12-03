2ec0dd011079f9b35b9776575f230f7c9e15d44e
Mickael Faivre-Maçon <faivrem@gmail.com>
2012-12-03 08:16:06 +0100

    fix

--- app/models/planning.rb
+++ app/models/planning.rb
@@ -7,7 +7,7 @@ class Planning < ActiveRecord::Base
     rv = []
     earlier = self.earlier_task.try(:start_date)
     latest  = self.latest_task.try(:end_date)
-    return rv if !earlier
+    return rv if !earlier or !latest
     current_date = earlier
     while(current_date <= latest)
       rv << [current_date, get_team_size_for_day(current_date)]
@@ -19,7 +19,7 @@ class Planning < ActiveRecord::Base
   def get_team_size_for_day(date)
     rv = 0
     self.tasks.each { |t|
-      rv += ((t.team_size / 0.1).round)*0.1 if date >= t.start_date and date < t.end_date
+      rv += ((t.team_size / 0.1).round)*0.1 if date >= t.start_date and (t.end_date and date < t.end_date)
       }
     rv
   end
@@ -32,7 +32,7 @@ class Planning < ActiveRecord::Base
 
   def latest_task
     rv = nil
-    self.tasks.each { |t| rv = t if !rv or t.end_date > rv.end_date }
+    self.tasks.each { |t| rv = t if !rv or (t.end_date and t.end_date > rv.end_date) }
     rv
   end
 
