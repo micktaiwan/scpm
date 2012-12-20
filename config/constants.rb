c5db02c2fb41a69cbf4dd7a0976f9d043806620c
cdebortoli <cdebortoli@sqli.com>
2012-12-20 16:00:18 +0100

    SDP Changes

--- config/constants.rb
+++ config/constants.rb
@@ -1,28 +1,28 @@
 # operational team weekly meetings
 # see /tools/import_monthly_tasks_form
-SDP_MONTHLY_IMPORT_Phase_ID       = 29255 # Bundle_Management
-SDP_RESP_MONTHLY_IMPORT_Activity_ID         = 127647 # Bundle_Management_during_the_project # not used
-SDP_OPERATIONAL_MONTHLY_IMPORT_Activity_ID  = 127649 # Operational Management
+SDP_MONTHLY_IMPORT_Phase_ID       = 32329 # Bundle_Management
+SDP_RESP_MONTHLY_IMPORT_Activity_ID         = 142237 # Bundle_Management_during_the_project # not used
+SDP_OPERATIONAL_MONTHLY_IMPORT_Activity_ID  = 142239 # Operational Management
 SDP_MONTHLY_IMPORT_Ref_Task_ID    = 110 # Encadrement
 SDP_MONTHLY_IMPORT_Macro_Task_ID  = -1
 SDP_MONTHLY_IMPORT_Workpackage_ID = -1
-SDP_MONTHLY_IMPORT_Iteration_ID   = 3056 # HO
+SDP_MONTHLY_IMPORT_Iteration_ID   = 3989 # HO
 SDP_MONTHLY_IMPORT_Entity_ID      = -1
 SDP_MONTHLY_IMPORT_Team_ID        = -1
 SDP_MONTHLY_IMPORT_Domain_ID      = -1
 SDP_MONTHLY_IMPORT_Agency_ID      = 13 # Toulouse
-SDP_MONTHLY_IMPORT_Proposal_Phase_ID = 28067 # Bundle Management
+SDP_MONTHLY_IMPORT_Proposal_Phase_ID = 31245 # Bundle Management
 SDP_MONTHLY_IMPORT_Comments       = "Internal SQLI Meetings (imported from BAM)"
 
-SDP_PROJECT_MANAGEMENT_MONTHLY_IMPORT_Phase_ID       = 29255 # Bundle_Management
-SDP_PROJECT_MANAGEMENT_MONTHLY_IMPORT_Activity_ID = 127647 # Bundle_Management_during_the_project 
+SDP_PROJECT_MANAGEMENT_MONTHLY_IMPORT_Phase_ID       = 32329 # Bundle_Management
+SDP_PROJECT_MANAGEMENT_MONTHLY_IMPORT_Activity_ID = 142237 # Bundle_Management_during_the_project 
 SDP_PROJECT_MANAGEMENT_MONTHLY_IMPORT_Ref_Task_ID = 112 # Suivi du projet
 SDP_PROJECT_MANAGEMENT_MONTHLY_IMPORT_Macro_Task_ID  = -1
 SDP_PROJECT_MANAGEMENT_MONTHLY_IMPORT_Workpackage_ID = -1
-SDP_PROJECT_MANAGEMENT_MONTHLY_IMPORT_Iteration_ID   = 3056 # Hors opérationnel
+SDP_PROJECT_MANAGEMENT_MONTHLY_IMPORT_Iteration_ID   = 3989 # Hors opérationnel
 SDP_PROJECT_MANAGEMENT_MONTHLY_IMPORT_Entity_ID      = -1
 SDP_PROJECT_MANAGEMENT_MONTHLY_IMPORT_Team_ID        = -1
 SDP_PROJECT_MANAGEMENT_MONTHLY_IMPORT_Domain_ID      = -1
 SDP_PROJECT_MANAGEMENT_MONTHLY_IMPORT_Agency_ID      = 13 # Toulouse
-SDP_PROJECT_MANAGEMENT_MONTHLY_IMPORT_Proposal_Phase_ID = 28067 # Bundle Management
+SDP_PROJECT_MANAGEMENT_MONTHLY_IMPORT_Proposal_Phase_ID = 31245 # Bundle Management
 SDP_PROJECT_MANAGEMENT_MONTHLY_IMPORT_Comments = "Internal SQLI Meetings (imported from BAM)"
