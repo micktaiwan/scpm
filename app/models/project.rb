e513948bb38b67270fc6190aa74f910e83f3665a
cdebortoli <cdebortoli@sqli.com>
2013-01-15 17:45:03 +0100

    Fix bug in milestone creation for projects.

--- app/models/project.rb
+++ app/models/project.rb
@@ -348,22 +348,23 @@ class Project < ActiveRecord::Base
   end
 
   def create_milestones
+    #LifecycleMilestone.find(:all, :conditions => ["lifecycle_id = ?",self.lifecycle_object.id]).each {|m| create_milestone(m.milestone_name.title)}
     case self.lifecycle_object.name
-    when FullGPP
-      ['M1', 'M3', 'QG BRD', 'QG ARD', 'M5', 'M7', 'M9', 'M10', 'QG TD', 'M10a', 'QG MIP', 'M11', 'M12', 'M13', 'M14'].each {|m| create_milestone(m)}
-    when LightGPP
-      ['M1', 'M3', 'QG BRD', 'QG ARD', 'M5/M7', 'M9/M10', 'QG TD', 'QG MIP', 'M11', 'M12/M13', 'M14'].each {|m| create_milestone(m)}
-    when Maintenance
-      ['CCB', 'QG TD M', 'MIPM'].each {|m| create_milestone(m)}
-    when LBIPGx
-      ['G0', 'G2', 'G3', 'G4', 'QG BRD', 'G5', 'G6', 'G7', 'G8', 'G9'].each {|m| create_milestone(m)}
-    when LBIPgx
-      ['g0', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8', 'g9'].each {|m| create_milestone(m)}
-    when LBIPpgx
-      ['pg0', 'pg2', 'pg3', 'pg4', 'pg5', 'pg6', 'pg7', 'pg8', 'pg9'].each {|m| create_milestone(m)}
-    when Suite
-      ['sM1', 'sM3', 'sM5', 'sM13', 'sM14'].each {|m| create_milestone(m)}
-    end
+        when "Full GPP"
+          ['M1', 'M3', 'QG BRD', 'QG ARD', 'M5', 'M7', 'M9', 'M10', 'QG TD', 'M10a', 'QG MIP', 'M11', 'M12', 'M13', 'M14'].each {|m| create_milestone(m)}
+        when "Light GPP"
+          ['M1', 'M3', 'QG BRD', 'QG ARD', 'M5/M7', 'M9/M10', 'QG TD', 'QG MIP', 'M11', 'M12/M13', 'M14'].each {|m| create_milestone(m)}
+        when "Maintenance"
+          ['CCB', 'QG TD M', 'MIPM'].each {|m| create_milestone(m)}
+        when "LBIP Gx"
+          ['G0', 'G2', 'G3', 'G4', 'QG BRD', 'G5', 'G6', 'G7', 'G8', 'G9'].each {|m| create_milestone(m)}
+        when "LBIP gx"
+          ['g0', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8', 'g9'].each {|m| create_milestone(m)}
+        when "LBIP pgx"
+          ['pg0', 'pg2', 'pg3', 'pg4', 'pg5', 'pg6', 'pg7', 'pg8', 'pg9'].each {|m| create_milestone(m)}
+        when "Suite"
+          ['sM1', 'sM3', 'sM5', 'sM13', 'sM14'].each {|m| create_milestone(m)}
+        end
   end
 
   def check
