09b8be49b292e11a020f11300d9c194d7bb4bf66
cdebortoli <cdebortoli@sqli.com>
2013-01-17 11:05:52 +0100

    Reference question interface update.

--- app/controllers/question_references_controller.rb
+++ app/controllers/question_references_controller.rb
@@ -2,18 +2,22 @@ class QuestionReferencesController < ApplicationController
   layout "tools"
   def index
     @axesList = PmTypeAxe.find(:all).map{|u| [u.title,u.id]}
+    @lifecycleList = Lifecycle.find(:all).map{ |l| [l.name, l.id]}
+    
     @firstAxeId = @axesList.first[1]
+    @firstLifecycleId = @lifecycleList.first[1]
   end
 
   def generate_question_references_table
     axe_id = params[:axe_id]
+    lifecycle_id = params[:lifecycle_id]
     @questionReferencesList = QuestionReference.find(:all,
       :joins => ["JOIN lifecycle_questions ON lifecycle_questions.id = question_references.question_id",
                  "JOIN pm_type_axes ON lifecycle_questions.pm_type_axe_id = pm_type_axes.id",
                  "JOIN pm_types ON pm_type_axes.pm_type_id = pm_types.id",
                  "JOIN lifecycles ON lifecycle_questions.lifecycle_id = lifecycles.id",
                  "JOIN milestone_names ON question_references.milestone_id = milestone_names.id"],
-      :conditions => ["pm_type_axes.id = ?",axe_id.to_s])
+      :conditions => ["pm_type_axes.id = ? and lifecycles.id = ?",axe_id.to_s, lifecycle_id.to_s])
       render(:layout=>false)
   end
   
