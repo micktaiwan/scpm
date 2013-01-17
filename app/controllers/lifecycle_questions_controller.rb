23112e786b75610c4fb9bfb4b10eed32815d2802
cdebortoli <cdebortoli@sqli.com>
2013-01-17 14:09:06 +0100

    Remove ajax for history navigation in lifecycle_question creation.

--- app/controllers/lifecycle_questions_controller.rb
+++ app/controllers/lifecycle_questions_controller.rb
@@ -6,7 +6,7 @@ class LifecycleQuestionsController < ApplicationController
     if params[:lifecycle_id] != nil
       @lifecycle_index_select = params[:lifecycle_id]
     else
-      @lifecycle_index_select = 0
+      @lifecycle_index_select = 1
     end
   end
   
