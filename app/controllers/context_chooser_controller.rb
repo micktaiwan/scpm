7db5a53d1f69428a626976b68ad7e257c0693c27
Emmanuel Ressouches <eressouches@sqli.com>
2013-04-08 15:13:59 +0200

    Display current actions from welcome panel

--- app/controllers/context_chooser_controller.rb
+++ app/controllers/context_chooser_controller.rb
@@ -6,5 +6,11 @@ class ContextChooserController < ApplicationController
   #  render(:text=>"/images/bg/01.jpg")
   #end
 
+  def index
+  	@current_actions_from_user = Action.find(:all, :conditions=>["person_id=? and progress in('open','in_progress')", current_user.id], :order=>"creation_date")
+  	@recent_current_actions_from_user = @current_actions_from_user.select { |action| (Time.now - action.creation_date) < 72}
+  end
+
+
 end
 
