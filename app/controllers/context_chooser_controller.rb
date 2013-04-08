5fc1479d2edc5ee7c810adc54c1229e565b76d79
Emmanuel Ressouches <eressouches@sqli.com>
2013-04-08 18:31:42 +0200

    Correction bug login

--- app/controllers/context_chooser_controller.rb
+++ app/controllers/context_chooser_controller.rb
@@ -8,7 +8,7 @@ class ContextChooserController < ApplicationController
 
   def index
   	@current_actions_from_user = Action.find(:all, :conditions=>["person_id=? and progress in('open','in_progress')", current_user.id], :order=>"creation_date")
-  	@recent_current_actions_from_user = @current_actions_from_user.select { |action| (Time.now - action.creation_date) < 72}
+  	#@recent_current_actions_from_user = @current_actions_from_user.select { |action| (Time.now - action.creation_date) < 72}
   end
 
 
