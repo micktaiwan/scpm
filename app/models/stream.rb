acff1d65d4e747850af63685b2d01bfe2035109b
cdebortoli <cdebortoli@sqli.com>
2013-07-10 11:42:11 +0200

    Suggested status calculated only on quality risks

--- app/models/stream.rb
+++ app/models/stream.rb
@@ -27,9 +27,11 @@ class Stream < ActiveRecord::Base
   def suggested_status
     rv = 1
     self.risks.each { |r|
-      rv = 2 if rv < 2 and (r.isMedium? or r.isHigh?)
-      rv = 3 if rv < 3 and r.isCritical?
-      }
+      if (r.is_quality == 1)
+        rv = 2 if rv < 2 and (r.isMedium? or r.isHigh?)
+        rv = 3 if rv < 3 and r.isCritical?
+      end
+    }
     rv
   end
 
