b895303467bb324c49b2d48e940e977eee116baf
Sof31t <laadhari.sofiene@gmail.com>
2013-07-25 17:34:12 +0200

    Add: projects filter

--- public/javascripts/project_workload.js
+++ public/javascripts/project_workload.js
@@ -34,10 +34,10 @@ Ajax.Replacer = Class.create(Ajax.Updater, {
   }
 })
 
-function change_workload(project_id) {
+function change_workload(project_ids) {
   document.body.style.cursor = 'wait';
   $('loading').show();
   new Ajax.Request('/project_workloads/change_workload', {
-    parameters: { project_id: project_id }
+    parameters: { project_ids: project_ids }
     });
   }
