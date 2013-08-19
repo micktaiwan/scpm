8dd8d603f242e8aabd3a8a5453102a3367e0a7a5
Sof31t <laadhari.sofiene@gmail.com>
2013-08-19 17:00:08 +0200

    Fix: color for export (project view) / Add: edit for company params / Add: delete holiday

--- app/models/wl_holiday.rb
+++ app/models/wl_holiday.rb
@@ -1,7 +1,7 @@
 class WlHoliday < ActiveRecord::Base
 
   def self.get_from_week(wlweek)
-    h = WlHoliday.find_by_week(wlweek)
+    h = WlHoliday.find_by_week(wlweek) #and_company
     return h.nb if h
     0
   end
