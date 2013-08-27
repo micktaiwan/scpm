0b6a95878ef9f09e57a1d0e24df1f5754ee8bcb3
Mickael Faivre-Maçon <faivrem@gmail.com>
2013-08-27 12:15:11 +0200

    fix

--- app/controllers/project_workloads_controller.rb
+++ app/controllers/project_workloads_controller.rb
@@ -26,7 +26,7 @@ class ProjectWorkloadsController < ApplicationController
         session['workload_companies_ids'] = [companies_ids] # array with one string
       end
     else
-      if session['workload_companies_ids'] == nil or session['workload_project_ids'] == ''
+      if session['workload_companies_ids'] == nil or session['workload_companies_ids'] == ''
         session['workload_companies_ids'] = []
       end
     end
