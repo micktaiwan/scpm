2011668126c4ff3518d4f55a7a00f8f4eff2441d
medbekkouch <bekkouchmohamed@gmail.com>
2014-06-18 10:59:18 +0100

    mantis:259 - Nouvel affichage des actions

--- app/controllers/context_chooser_controller.rb
+++ app/controllers/context_chooser_controller.rb
@@ -7,7 +7,10 @@ class ContextChooserController < ApplicationController
   #end
 
   def index
+    now = DateTime.now
   	@current_actions_from_user = Action.find(:all, :conditions=>["person_id=? and progress in('open','in_progress')", current_user.id], :order=>"creation_date")
+  	@actions      = Action.find(:all, :conditions=>["person_id=? and progress in('open','in_progress')", current_user.id])
+  	@action_48hr  = Action.find(:all, :conditions=>["person_id=? and progress in('open','in_progress') and updated_at >= ?", current_user.id, now - 48.hours])
   	#@recent_current_actions_from_user = @current_actions_from_user.select { |action| (Time.now - action.creation_date) < 72}
   end
 
